1a6a12633672030b4f14a64336dd4aad54a40de3
Sizenando França <99983803+sizenando-f@users.noreply.github.com>
2024-03-29 13:33:25 -0400

    Update of attempt of new train of thought

--- C - C++/cos.c
+++ C - C++/cos.c
@@ -3,7 +3,7 @@
 
 double fat(int n) {
     if(n == 1){
-      return 1;
+        return 1;
     }
 
     return n * fat(n - 1);
@@ -12,13 +12,16 @@ double fat(int n) {
 double cosen(int x, int n) {
     double res = 0.0;
     int exp = 2;
+    int sinal = 1;
 
     for (int i = 1; i <= n; i++) {
-        if (i % 2 == 0) {
-            res += pow(x, exp) / fat(exp);
-        } else {
-            res -= pow(x, exp) / fat(exp);
-        }
+        res += sinal * pow(x, exp) / fat(exp);
+        sinal *= -1;
+        // if (i % 2 == 0) {
+        //     res += pow(x, exp) / fat(exp);
+        // } else {
+        //     res -= pow(x, exp) / fat(exp);
+        // }
 
         exp += 2;
     }
@@ -27,8 +30,8 @@ double cosen(int x, int n) {
 }
 
 int main() {
-    double x = 0 * M_PI / 180;
-    double resultado = cosen(x, 20);
+    double x = 270 * M_PI / 180;
+    double resultado = cosen(x, 100);
     printf("Cosseno de %f graus: %f\n", x, resultado);
 
     return 0;
