d8c5eed721a6d3e31162876f3c6d40a9ee67ed0e
Sizenando França <99983803+sizenando-f@users.noreply.github.com>
2024-04-06 13:40:44 -0400

    Support implemetation of big entries of degress and radian

--- C - C++/cos.c
+++ C - C++/cos.c
@@ -12,15 +12,34 @@ double fat(int n) {
 
 double to_radian(double x) {return (x * M_PI )/ 180;}; 
 
+double fix_dg(double dg){
+    while(dg > 360){
+        dg -= 360;
+    }
+
+    return dg;
+}
+
+double fix_radian(double radian){
+    while(radian > 6.283185307179586){
+        radian -= 6.283185307179586;
+    }
+
+    return radian;
+}
+
 double cosen_rad(double rad, int n) {
     double res = 1.0;
     int exp = 2;
 
-    if(rad == 1.570779 || rad == 4.712388){
+    rad = rad < 0 ? rad*(-1) : rad;
+    rad = fix_radian(rad);
+
+    if(rad == 1.570796326794896 || rad == 4.712388980384689){
         return 0.0;
-    } else if(rad == 0.0 || rad == 6.283185){
+    } else if(rad == 0.0 || rad == 6.283185307179586){
         return 1.0;
-    } else if(rad == 3.141592){
+    } else if(rad == 3.141592653589793){
         return -1.0;
     }
 
@@ -53,11 +72,11 @@ double cosen_dg(double dg, int n) {
     double res = 1.0;
     int exp = 2;
 
-    if(dg == 90 || dg == 270){
+    if(dg == 90.0 || dg == 270.0){
         return 0.0;
-    } else if(dg == 0 || dg == 360){
+    } else if(dg == 0.0 || dg == 360.0){
         return 1.0;
-    } else if(dg == 180){
+    } else if(dg == 18.0){
         return -1.0;
     }
 
@@ -73,7 +92,9 @@ double cosen_dg(double dg, int n) {
         n = 1;
     }
 
-    double x = to_radian(dg);
+    dg = dg < 0 ? dg*(-1) : dg;
+    double fixed_dg = fix_dg(dg);
+    double x = to_radian(fixed_dg);
 
     for (int i = 1; i <= n; i++) {
         if (i % 2 == 0) {
@@ -133,22 +154,15 @@ int main() {
                 }while(n == 0);
                 resultado = cosen_dg(dg, n);
                 printf("\033[0;32m");
-                printf("\n[ -> ] Cosseno de \033[4;32m%.2f\033[0;32m graus:\033[1;32m %f\n\n", dg, resultado);
+                printf("\n[ -> ] Cosseno de \033[4;32m%.2f\033[0;32m graus:\033[1;32m %.15f\n\n", dg, resultado);
                 printf("\033[0;37m");
                 break;
             case 2:
-                do{
-                    printf("[ <- ] Insira o valor em radiano (ex.: 1.570796) > ");
-                    getchar();
-                    printf("\033[1;34m");
-                    scanf("%f", &x);
-                    printf("\033[0;37m");
-                    if(x > 34 || x < -34){
-                        printf("\033[0;31m");
-                        printf( "[ #<- ] ERRO: Insira um valor entre -34 e 34\n");
-                        printf("\033[0;37m");
-                    }
-                }while(x > 34);
+                printf("[ <- ] Insira o valor em radiano (ex.: 1.570796) > ");
+                getchar();
+                printf("\033[1;34m");
+                scanf("%f", &x);
+                printf("\033[0;37m");
                 do{
                     printf("[ <- ] Insira o valor de N (max.: 100) > ");
                     getchar();
@@ -163,7 +177,7 @@ int main() {
                 } while(n == 0); 
                 resultado = cosen_rad(x, n);
                 printf("\033[0;32m");
-                printf("\n[ -> ] Cosseno de \033[4;32m%f\033[0;32m radianos:\033[1;32m %f\n\n", x, resultado);
+                printf("\n[ -> ] Cosseno de \033[4;32m%f\033[0;32m radianos:\033[1;32m %.15f\n\n", x, resultado);
                 printf("\033[0;37m");
                 break;
             case 3:
