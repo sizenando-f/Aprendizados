e508c809b09af3416c4af9d922868432e50ac9a4
Sizenando França <99983803+sizenando-f@users.noreply.github.com>
2024-04-18 16:23:22 -0400

    Inserção de realocação de memória e remoção de elementos

--- C - C++/arraydinamicoordenado.cpp
+++ C - C++/arraydinamicoordenado.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <cstring>
 
 using namespace std;
 
@@ -7,96 +8,120 @@ class OrderedDinamicArray {
   unsigned nElem; // Número de elementos
   int *_data; // Ponteiro para vetor
 
-  public:
-    OrderedDinamicArray(): size(2), _data(new int[size]), nElem(0){
-    }
-
-    ~OrderedDinamicArray(){
-      if(_data) delete []_data;
-    }
-
-    unsigned getSize() const {
-      return size;
-    }
-
-    unsigned getNElem() const {
-      return nElem;
-    }
-
-    int at(unsigned i) const {
-      return _data[i];
-    }
-
-    bool search(int key, unsigned &index){
-      index = 0;
-      while(key < nElem){
-        if(key == _data[index]){
-          return true;
-        }
-        index++
-      }
-
-      return false;
-    }
-
-    bool search(int key){
-      unsigned none;
-      return search(key, none);
-    }
-
-    void insert(int elem){
-      unsigned i;
-      if(nElem < size){
-        i = nElem;
-        _data[nElem++] = elem;
-        while(_data[i] < _data[i-1] && i > 0){
-          swap(_data[i], _data[--i]); // Testar --i
-        }
-      } else {
-        // Redimensionar o array
-      }
-    }
-
-    void removeMax(){
-      nElem = nElem ? nElem-1 : 0;
-      // Tratar o redimensionamento
-    }
-
-    void removeMin(){
-      unsigned i;
-      i = 0;
-      --nElem;
-      while(i < nElem - 1){
-        _data[i] =  _data[++i]; // Testar ++i
-      }
-      // Tratar o redimensionamento
+  void resize(unsigned newSize){
+    int *pAux = new int[newSize];
+    memcpy(pAux, _data, nElem * sizeof(int));
+    delete[] _data;
+    _data = pAux;
+    size = newSize;
+  }
+  
+  void mySwap(int &a, int &b){
+      register int aux;
       
-    }
+      aux = a;
+      a = b;
+      b = aux;
+  }
 
-    void removeFromKey(int key){
-      unsigned i;
-      if(search(key, i)){
-        --nElem;
-        while(i < nElem - 1){
-          _data[i] =  _data[++i]; // Testar ++i
-        }
+public:
+  OrderedDinamicArray(): size(2), _data(new int[size]), nElem(0){
+  }
+
+  ~OrderedDinamicArray(){
+    if(_data) delete []_data;
+  }
+
+  unsigned getSize() const {
+    return size;
+  }
+
+  unsigned getNElem() const {
+    return nElem;
+  }
+
+  int at(unsigned i) const {
+    return _data[i];
+  }
+
+  bool search(int key, unsigned &index){
+    index = 0;
+    while(index < nElem){
+      if(key == _data[index]){
+        return true;
       }
-      // Tratar o redimensionamento
+      index++;
     }
+    return false;
+  }
 
+  bool search(int key){
+    unsigned none;
+    return search(key, none);
+  }
+
+  void insert(int elem){
+    unsigned i;
+    if(nElem == size){
+      resize(size*2);
+    }
+      i = nElem;
+      _data[nElem++] = elem;
+      while(i > 0 && elem < _data[i-1]){
+        mySwap(_data[i], _data[i-1]); // Trocar elementos para manter a ordem
+        i--;
+      }
+  }
+
+  void removeMax(){
+    nElem = nElem ? nElem-1 : 0;
+    if(size > 0 && size-nElem > size/2){
+        resize(size/2);
+    }
+  }
+
+  void removeMin(){
+    if(nElem > 0){
+      for(unsigned i = 0; i < nElem - 1; ++i){
+        _data[i] =  _data[i + 1];
+      }
+      --nElem;
+      if(size > 0 && size-nElem > size/2){
+        resize(size/2);
+    }
+    }
+  }
+
+  void removeFromKey(int key){
+    unsigned i;
+    if(search(key, i)){
+      for(i=key-1; i < nElem - 1; ++i){
+        _data[i] =  _data[i + 1];
+      }
+      --nElem;
+     if(size > 0 && size-nElem > size/2){
+        resize(size/2);
+    }
+    }
+  }
 };
 
 int main(){
   int i;
-  OrderedDinamicArray vet(5);
+  OrderedDinamicArray vet;
 
-  for(i = 0; i < 5; i++){
-    vet.at(i) = i + 1;
+  for(i = 5; i > 0; i--){
+    vet.insert(i);
   }
+  
+  vet.removeMin();
+  vet.removeMin();
 
-  for(i = 0; i < 5; i++){
+  for(i = 0; i < vet.getNElem(); i++){
     cout << vet.at(i) << ' ';
   }
+  
+  cout << endl << vet.getSize();
 
   return 0;
 }
