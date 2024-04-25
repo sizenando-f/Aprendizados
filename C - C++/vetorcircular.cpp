ce6b513a0927877f168c05fadd7f35c4ee2c7d88
Sizenando França <99983803+sizenando-f@users.noreply.github.com>
2024-04-25 14:05:40 -0400

    Insertion of get functions

--- C - C++/vetorcircular.cpp
+++ C - C++/vetorcircular.cpp
@@ -4,11 +4,23 @@ using namespace std;
 
 class circularDinamicList(){
   int *ptr;
-  int head, tail;
+  int front, back;
   unsigned blockSize;
 
   public:
-    circularDinamicList(): blockSize(2), head(0), tail(0), ptr(new int(blockSize));
+    circularDinamicList(): blockSize(2), front(0), back(0), ptr(new int(blockSize)){};
+
+    bool isEmpty() const {
+      return front == back;
+    }
+
+    int getFront() const {
+      return ptr[front];
+    }
+
+    int getBack() const {
+      return ptr[back];
+    }
 };
 
 int main(){
