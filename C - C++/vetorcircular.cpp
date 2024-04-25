2ae16d550aadb3cd3d01534fa909f228b7202371
Sizenando França <99983803+sizenando-f@users.noreply.github.com>
2024-04-25 15:05:37 -0400

    Insertion o push and pop functions

--- C - C++/vetorcircular.cpp
+++ C - C++/vetorcircular.cpp
@@ -7,6 +7,10 @@ class circularDinamicList(){
   int front, back;
   unsigned blockSize;
 
+  void resizeBlock(unsigned newSize){
+
+  }
+
   public:
     circularDinamicList(): blockSize(2), front(0), back(0), ptr(new int(blockSize)){};
 
@@ -20,6 +24,44 @@ class circularDinamicList(){
 
     int getBack() const {
       return ptr[back];
+    }
+
+    void pushFront(int elem){
+      if((back+1) % blockSize == front){
+        resizeBlock(blockSize*2);
+      }
+      // front = (front - 1 + blockSize) % blockSize;
+
+      front = front == 0 ? blockSize - 1 : front - 1;
+      
+      ptr[front] = elem;
+    }
+    
+    void pushBack(int elem){
+      if((back+1) % blockSize == front){
+        resizeBlock(blockSize*2);
+      }
+
+      ptr[back] = elem;
+
+      back = (back + 1) % blockSize; //! ERRO AQUI, SEMPRE VAI RESETAR O BACK QUANDO PUSHBACK FOR CHAMADO MESMO QUE NÃO ESTEJA NO FIM
+    }
+
+    bool popFront(){
+      int nElem = 0;
+      if(front < back){ 
+        nElem = back - front
+      } else if(front > back){
+        nElem = blockSize - (front - back);
+      }
+
+      if(blockSize/4 > nElem){
+        resizeBlock(blockSize/2);
+      }
+    }
+
+    bool popBack(){
+
     }
 };
 
