513c707cdd6e05a2ab60e5c55fe6e7ba57fee82b
Sizenando França <99983803+sizenando-f@users.noreply.github.com>
2024-05-09 09:40:01 -0400

    Insertion of elements in the ordered array

--- C - C++/Lista/ordenadosequencial.c
+++ C - C++/Lista/ordenadosequencial.c
@@ -2,20 +2,46 @@
 
 void insereElemento(int vet[], int elem, int *n){
   int tam = 10;
+  int maior = 0;
+
   if(*n == 0){
     vet[*n] = elem;
-    *n++;
+    *n = *n + 1;
+    return;
+  } else if(*n == tam){
+    printf("Vetor cheio");
     return;
   }
 
   for(int i = 0; i < *n; i++){
-    
+    if(vet[i] > elem){
+      maior = i;
+      i = *n;
+    } else if(vet[i] < elem && *n - 1 == i){
+      vet[i+1] = elem;
+      *n = *n + 1;
+      return;
+    }
   }
+
+  for(int i = tam-1; i > maior; i--){
+    vet[i] = vet[i-1];
+  }
+
+  vet[maior] = elem;
+  *n = *n + 1;
 }
 
 int main(){
   int vet[10];
   int n = 0;
   insereElemento(vet, 0, &n);
+  insereElemento(vet, 2, &n);
+  insereElemento(vet, 4, &n);
+  insereElemento(vet, 1, &n);
+  insereElemento(vet, 3, &n);
+  for(int i = 0; i < n; i++){
+    printf("%d -> ", vet[i]);
+  }
   return 0;
 }
